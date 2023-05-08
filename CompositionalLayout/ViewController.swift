b0b209ebae02d1115b28e2f5af571b05547c6e9f
MikhailEgorov <egorovmail25gmail.com>
2023-05-08 13:05:05 +0300

    Initial Commit

---
+++ CompositionalLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompositionalLayout
+//
+//  Created by Егоров Михаил on 08.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
